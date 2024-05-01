9c12468400905457aaaf674c68f8c625ad1a62f6
Priyanka Yadav <pvrano@gmail.com>
2024-05-01 12:22:54 +0530

    first commit

---
+++ PracticeOops/CountDuplicateLetters.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeOops
+{
+    public class CountDuplicateLetters
+    {
+        public static void Main(String[] args)
+        {
+            String sentence = "this is a repeat type word sentence";
+
+            Dictionary<char, int> countLetter = countingLetters(sentence);
+            Console.WriteLine("Here is the number of time a letter occured");
+            foreach (var pair in countLetter)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+
+        static Dictionary<char, int> countingLetters(string sentence)
+        {
+            Dictionary<char, int> countLetter = new Dictionary<char, int>();
+            foreach (char c in sentence)
+            {
+                if (char.IsLetter(c))
+                {
+                    char lowercaseChar = char.ToLower(c);
+                    if (countLetter.ContainsKey(lowercaseChar))
+                    {
+                        countLetter[lowercaseChar]++;
+                    }
+                    else
+                    {
+                        countLetter[lowercaseChar] = 1;
+                    }
+                }
+
+            }
+            return countLetter;
+
+
+
+        }
+    }
+}
---
+++ PracticeOops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeOops
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HowManyTimes.Main(args);
+            practiceArray.Main(args);
+            CountDuplicateLetters.Main(args);
+        }
+    }
+}
